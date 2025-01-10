273d904985e4ccb1f506990f3538286483f066d7
Marian <mardidukh@gmail.com>
2025-01-10 18:09:00 +0100

    feat: add --version flag

--- src/gitpulse/cli.py
+++ src/gitpulse/cli.py
@@ -1,9 +1,16 @@
 ﻿import typer
 
-app = typer.Typer()
+app = typer.Typer(name="gitpulse")
+
+def _version_callback(value: bool) -> None:
+    if value:
+        print("GitPulse v0.1.0")
+        raise typer.Exit()
 
 @app.command()
-def hello():
+def hello(
+    version: bool = typer.Option(None, "--version", "-v", callback=_version_callback, is_eager=True),
+):
     print("Hello from GitPulse!")
 
 if __name__ == "__main__":
