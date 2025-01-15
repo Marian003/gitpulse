6fc06c412c8e3c929c5354136a6de40e70dcc7f5
Marian <mardidukh@gmail.com>
2025-01-15 16:25:00 +0100

    feat: token resolution from env

--- src/gitpulse/config.py
+++ src/gitpulse/config.py
@@ -1,4 +1,5 @@
-﻿from dataclasses import dataclass
+﻿import os
+from dataclasses import dataclass
 from typing import Optional
 
 ENV_TOKEN_KEY = "GITHUB_TOKEN"
@@ -6,3 +7,8 @@ ENV_TOKEN_KEY = "GITHUB_TOKEN"
 @dataclass
 class Config:
     token: Optional[str] = None
+
+def resolve_token(token_flag: Optional[str] = None) -> Optional[str]:
+    if token_flag:
+        return token_flag
+    return os.environ.get(ENV_TOKEN_KEY) or None
