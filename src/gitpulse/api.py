9d1a6c259d05de19976b68d818cbff0e384d94e2
Marian <mardidukh@gmail.com>
2025-01-21 08:37:00 +0100

    feat: GitHub API client skeleton

---
+++ src/gitpulse/api.py
@@ -0,0 +1,8 @@
+﻿from typing import Any, Optional
+import httpx
+from gitpulse.config import Config
+
+class GitHubClient:
+    def __init__(self, config: Config) -> None:
+        self._config = config
+        self._client: Optional[httpx.AsyncClient] = None
